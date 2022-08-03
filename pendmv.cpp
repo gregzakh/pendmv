2328601cecf3e5efd4e79ae664ad6aea96b56d9e
gregzakh <grishanz@yandex.com>
2022-08-03 23:34:17 +0500

    M pendmv.cpp

--- pendmv.cpp
+++ pendmv.cpp
@@ -3,31 +3,23 @@
 
 int main(void) {
   HKEY    rk{};
-  auto  ls{0l};
+  winerr err{};
   auto sz{0ul};
 
   std::unique_ptr<HKEY, AutoDeleter<HKEY, AuRegCloseKey>> key(
-    ((ls = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+    ((err.set(::RegOpenKeyEx(HKEY_LOCAL_MACHINE,
       L"SYSTEM\\CurrentControlSet\\Control\\Session Manager",
       0, KEY_QUERY_VALUE, &rk
-    )), rk)
+    ))), rk)
   );
-  if (ERROR_SUCCESS != ls) {
-    getlasterror(ls);
-    return 1;
-  }
+  if (err.get()) return 1;
 
-  ls = queryvalue(rk, L"PendingFileRenameOperations", nullptr, &sz);
-  if (ERROR_SUCCESS != ls) {
-    getlasterror(ls);
-    return 1;
-  }
+  err.set(queryvalue(rk, L"PendingFileRenameOperations", nullptr, &sz));
+  if (err.get()) return 1;
+
   std::vector<BYTE> buf(sz);
-  ls = queryvalue(rk, L"PendingFileRenameOperations", &buf[0], &sz);
-  if (ERROR_SUCCESS != ls) {
-    getlasterror(ls);
-    return 1;
-  }
+  err.set(queryvalue(rk, L"PendingFileRenameOperations", &buf[0], &sz));
+  if (err.get()) return 1;
 
   auto res = reinterpret_cast<PWSTR>(&buf[0]);
   while (1) {
@@ -42,17 +34,11 @@ int main(void) {
   buf.shrink_to_fit();
   buf.clear();
 
-  ls = queryinfo(rk, buf);
-  if (ERROR_SUCCESS != ls) {
-    getlasterror(ls);
-    return 1;
-  }
+  err.set(queryinfo(rk, buf));
+  if (err.get()) return 1;
 
-  ls = ftime2stime(buf);
-  if (ERROR_SUCCESS != ls) {
-    getlasterror(ls);
-    return 1;
-  }
+  err.set(ftime2stime(buf));
+  if (err.get()) return 1;
   auto tm = *reinterpret_cast<WORD(*)[8]>(&buf[0]);
   printf(
     "Time of last update to pending move key: %.2hu/%.2hu/%.4hu %.2hu:%.2hu:%.2hu\n",
